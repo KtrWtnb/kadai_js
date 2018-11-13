4ab52d667abe2442691f82854352538be4ad501a
KtrWtnb <whatnowbay.k3nx2@gmail.com>
2018-11-13 12:46:02 +0900

    horizontal-side fin

--- js/main.js
+++ js/main.js
@@ -1,32 +1,40 @@
 $(function() {
+    $("#home-a").addClass("current");
     $(".global-nav a").click(function() {
         var current = $(this).attr("href");
-        if (current != "#home") {
+        if ($(current).hasClass("other")) {
 
             if (!$(current).hasClass("active")) {
 
+                activeRemove(".active", 300, 280);
+                $(".current").removeClass("current");
+                $(this).addClass("current");
                 $(current).addClass("active");
-                $(".main-container").append('<div id="bg">');
-                $(current).stop().slideDown();
+                $(".main-container").append('<div class="bg">');
+                $(current).show(500);
+                $(".bg").show(400);
 
             } else {
-
-                $(current).slideUp();
-                $(current).removeClass("active");
-                $("#bg").slideUp(function() {
-                    $(this).remove();
-                });
+                activeRemove(current, 500, 300);
             }
-
         } else {
-            $(".current").slideUp(function() {
-                $(".current").removeClass("active");
-            });
-            $("#bg").slideUp(function() {
-                $(this).remove();
-            });
+            activeRemove(".active", 500, 300);
         }
-
+        $(".bg").click(function() {
+            activeRemove(".active", 500, 300);
+        });
         return false;
     });
+
+    function activeRemove(target, tHide, bHide) {
+        $(target).hide(tHide, function() {
+            $(this).removeClass("active");
+        });
+        $(".bg").hide(bHide, function() {
+            $(this).remove();
+        });
+        $(".current").removeClass("current");
+        $("#home-a").addClass("current");
+    }
+
 });
