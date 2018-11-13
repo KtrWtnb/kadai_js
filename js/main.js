0955ae6f7dedd7b56a9a13bc1e4afcf40c513b4d
KtrWtnb <whatnowbay.k3nx2@gmail.com>
2018-11-13 17:30:19 +0900

    fixed-gloMenu fin

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 $(function() {
+    var navPos = $(".sub-nav").offset().top - 61;
+    
+    $(window).scroll(function() {
+        var togglePos = $(window).scrollTop();
+        // if ($(window).scrollTop() > navPos) {
+        if (togglePos > navPos) {
+            $(".global-nav").addClass("scroll");
+            $(".sub-nav").addClass("scroll");
+        } else {
+            $(".global-nav").removeClass("scroll");
+            $(".sub-nav").removeClass("scroll");
+        }
+    });
+
+    // horizontal-slide
     $("#home-a").addClass("current");
     $(".global-nav a").click(function() {
         var current = $(this).attr("href");
@@ -6,6 +21,7 @@ $(function() {
 
             if (!$(current).hasClass("active")) {
 
+                pageScr(140);
                 activeRemove(".active", 300, 280);
                 $(".current").removeClass("current");
                 $(this).addClass("current");
@@ -15,16 +31,23 @@ $(function() {
                 $(".bg").show(400);
 
             } else {
+                pageScr(0);
                 activeRemove(current, 500, 300);
             }
         } else {
+            pageScr(0);
             activeRemove(".active", 500, 300);
         }
         $(".bg").click(function() {
+            pageScr(0);
             activeRemove(".active", 500, 300);
         });
         return false;
     });
+
+    function pageScr(pos) {
+        $("html, body").animate({scrollTop: pos}, 300);
+    }
 
     function activeRemove(target, tHide, bHide) {
         $(target).hide(tHide, function() {
